a4371520daf5fdb0dcf34004581fff58de260498
Janusheks <gaujenieyis@gmail.com>
2023-01-27 08:47:25 +0200

    Pirmais solitis projektaa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projektgg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
